aa4fe2593bc31e1d2d0707abe938fe1e83355d76
speedracer05 <speedracer-05@outlook.com>
2021-09-07 19:58:17 -0700

    add parse data, scale functions for x-y axis, append axis to charts for x-y

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -1,56 +1,77 @@
-  // https://www.d3-graph-gallery.com/graph/bubble_tooltip.html
-
-  // set the dimensions and margins of the graph
-  // source: http://www.d3noob.org/2012/12/setting-up-margins-and-graph-area.html
+  // Set dimensions and margins of svg chart area
+  // ********************************************
 var margin = {top: 80, right: 20, bottom: 80, left: 50},
-    width = 400 - margin.left - margin.right,
-    healthcare = 270 - margin.top – margin.bottom;
+  width = 400 - margin.left - margin.right,
+  height = 270 - margin.top - margin.bottom;
 
-  // append the svg object to the body of the page
-var svg = d3.select("#scatter")
+  // Append the svg object to the body of the page and set its margins
+var svg = d3
+  .select("#scatter")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("healthcare", healthcare + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g") // Append a group area and set margins
+  .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
 
-  //Read the data
-d3.csv("./D3_data_journalism/static/data/data.csv").then(function(data) {
+
+  //Read census data
+d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
+  // Print data
+console.log(censusData)
+
   // Visualize the data
-  visualize(data);
+  visualize(censusData);
 
-  // Add circle for each item in the array
-  var circles = d3.select("#scatter").selectAll("circle")
-  .data(data);
+  // Parse data converting strings to numbers
+censusData.forEach(function(data) {
+  data.poverty = +data.poverty;
+  data.healthcare = +data.healthcare;
+  // data.income = +data.income;    // Save for future update to dynamic scatter plot 
+  // data.obesity = +data.obesity;  // ""
+  // data.smokes = +data.smokes;    // ""
+  // data.age = +data.age;          // ""
+});
 
-circles.enter().append("circle")
-  .attr("cx", function(d, i){ return 25 + (50 * i); })
-  .attr("cy", function(d, i){ return 25 + (50 * i); })
-  .attr("r", 5)
-  .attr("fill", "grey");
-
-  // Add X axis
+  // Create scale functions  (x and y axis)
 var x = d3.scaleLinear()
-  .domain([d3.min(data, function(d){ return d.poverty; }) / 1.05, 
-    d3.max(data, function(d){ return d.poverty; }) * 1.05])
-.range([0, 800]);
-svg.append("g")
-  .attr("transform", "translate(0," + healthcare + ")")
-  .call(d3.axisBottom(x));
+  .domain([d3.min(censusData, function(d){ return d.poverty; }) / 1.05, 
+  d3.max(censusData, function(d){ return d.poverty; }) * 1.05])
+  .range([0, 800]);
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([d3.min(data, function(d){ return d.healthcare; }) / 1.05,
-      d3.max(data, function(d){ return d.healthcare; }) * 1.05])
+var y = d3.scaleLinear()
+  .domain([d3.min(censusData, function(d){ return d.healthcare; }) / 1.05,
+  d3.max(censusData, function(d){ return d.healthcare; }) * 1.05])
   .range([500, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+
+  // Append axes to chart
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+svg.append("g")
+  .call(d3.axisLeft(y));
+
+
+    // Add circle for each item in the array
+    var circles = d3
+    .select("#scatter")
+    .selectAll("circle")
+    .data(data);
+
+    circles
+    .enter()
+    .append("circle")
+    .attr("cx", function(d){ return x(d.poverty); })
+    .attr("cy", function(d, i){ return y(d.healthcare); })
+    .attr("r", 16)
+    .attr("fill", "#4FC3F7")
+    .att("opacity", ".5");
+
 
   // Add dots
   svg.append('g')
     .selectAll("dot")
-    .data(data)
+    .data(censusData)
     .enter()
     .append("circle")
       .attr("cx", function (d) { return x(d.healthcare); } )
