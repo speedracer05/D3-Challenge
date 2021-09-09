0525d2f80f3c185156270a397215518c6b5eb261
speedracer05 <speedracer-05@outlook.com>
2021-09-08 17:38:06 -0700

    Bug. Tooltips appears off the chart-hidden. Rearranging code to see if it makes a difference

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -1,20 +1,19 @@
   // Set dimensions and margins of svg chart area
-  // ********************************************
-var margin = {top: 80, right: 20, bottom: 80, left: 50},
+var margin = {top: 20, right: 40, bottom: 85, left: 100},
   width = 900 - margin.left - margin.right,
   height = 600 - margin.top - margin.bottom;
 
-  // Create an SVG wrapper, append an svg group to hold the chart and set its margins
+  // Create an SVG wrapper; append an svg group to hold the chart and set its margins
 var svg = d3
   .select("#scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-
+    .append("g") 
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
 // Append a group area and set margins
 var chartGroup = svg
-  .append("g") 
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   //Read census data
 d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
@@ -50,7 +49,7 @@ d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
   var chartGroup = svg.append("g")
     .call(d3.axisLeft(y));
 
-  // Step 4: Create Circles and event listner to show/hide the tooltip
+  // Step 4: Create Circles
   // ==============================
   var circlesGroup = chartGroup.selectAll("circle")
     .data(censusData)
@@ -60,7 +59,7 @@ d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
     .attr("cy", d => y(d.healthcare))
     .attr("r", "15")
     .attr("fill", "#4FC3F7")
-    .classed("stateCircle", true)
+    // .classed("stateCircle", true)  // MARK FOR REMOVAL
     .attr("opacity", ".5");
 
 // Step 6: Initialize tool tip
@@ -89,30 +88,14 @@ d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
       toolTip.hide(data);
     });
 
-  // Create axes labels
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .text("Healthcare");
-
-  chartGroup.append("text")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    .attr("class", "axisText")
-    .text("Poverty (%)");
-
-    // Create circle text
   var circlesText = chartGroup.selectAll("abbr")
     .data(censusData)
     .enter()
     .append("text")
     .text(d => d.abbr);
     console.log(circlesText);
-  });
 
-
+  // Create circle text
   var textGroup = chartGroup.selectAll("abbr")
   .data(censusData)
   .enter()
@@ -123,6 +106,24 @@ d3.csv("./D3_data_journalism/static/data/data.csv").then(function(censusData) {
   .text(d => d.abbr)
   .attr("dominate-baseline", "middle")
   .attr("text-anchor", "middle");
+
+    // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Poverty (%)");
+  
+}).catch(function(error) {
+  console.log(error);
+});
 
 
 
